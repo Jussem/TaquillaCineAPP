e954e46f98ff645807c2c327e3ad8a927cea838d
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-05 23:08:19 -0500

    Se comento el codigo de la clase 'Adulto' y 'Boleta'

--- src/autonoma/TaquillaCineAPP/models/Adulto.java
+++ src/autonoma/TaquillaCineAPP/models/Adulto.java
@@ -5,15 +5,33 @@
 package autonoma.TaquillaCineAPP.models;
 
 /**
+ * Representa un usuario adulto dentro del sistema.
+ * Este tipo de usuario no cuenta con descuento en la compra de boletas.
  *
  * @author marti
+ * @version 20250405
+ * @since 1.0
+ * @see Usuario
  */
 class Adulto extends Usuario {
+
+    /**
+     * Crea un nuevo usuario adulto con el nombre especificado.
+     *
+     * @param nombre Nombre del usuario adulto.
+     */
     public Adulto(String nombre) {
         super(nombre);
     }
 
+    /**
+     * Calcula el porcentaje de descuento aplicable al usuario adulto.
+     * Los adultos no tienen descuento, por lo tanto retorna 0.0.
+     *
+     * @return Porcentaje de descuento como valor decimal (0.0 representa 0%).
+     */
     public double calcularPorcentajeDescuento() {
         return 0.0;
     }
 }
+
--- src/autonoma/TaquillaCineAPP/models/Boleta.java
+++ src/autonoma/TaquillaCineAPP/models/Boleta.java
@@ -7,15 +7,39 @@ package autonoma.TaquillaCineAPP.models;
 import autonoma.TaquillaCineAPP.Exception.PrecioNegativoException;
 
 /**
- *
+ * Representa una boleta de cine asociada a una película, una función y un usuario.
+ * La boleta calcula automáticamente su precio final al ser creada,
+ * teniendo en cuenta posibles descuentos por tipo de usuario y función.
+ * 
  * @author juanb
+ * @version 20250405
+ * @since 1.0
+ * @see Pelicula
+ * @see Funcion
+ * @see Usuario
  */
 class Boleta {
+
+    /** Película asociada a la boleta */
     private Pelicula pelicula;
+
+    /** Función en la que se proyecta la película */
     private Funcion funcion;
+
+    /** Usuario que adquiere la boleta */
     private Usuario usuario;
+
+    /** Precio final de la boleta luego de aplicar descuentos */
     private double precioFinal;
 
+    /**
+     * Crea una nueva boleta con la información especificada.
+     * Calcula automáticamente el precio final al momento de la creación.
+     *
+     * @param pelicula Película que se va a proyectar.
+     * @param funcion Función en la que se proyecta la película.
+     * @param usuario Usuario que adquiere la boleta.
+     */
     public Boleta(Pelicula pelicula, Funcion funcion, Usuario usuario) {
         this.pelicula = pelicula;
         this.funcion = funcion;
@@ -23,25 +47,43 @@ class Boleta {
         this.precioFinal = calcularPrecio();
     }
 
+    /**
+     * Calcula el precio final de la boleta considerando descuentos por función y usuario.
+     *
+     * @return Precio final de la boleta.
+     * @throws PrecioNegativoException Si el precio resultante es menor que cero.
+     */
     public double calcularPrecio() {
-    double base = pelicula.getCostoBase();
-    double descuentoFuncion = base * funcion.calcularPorc();
-    double descuentoUsuario = usuario.calcularPorcentajeDescuento();
-    double precio = base - descuentoFuncion - descuentoUsuario;
-    
-    if (precio < 0) {
-        throw new PrecioNegativoException();
+        double base = pelicula.getCostoBase();
+        double descuentoFuncion = base * funcion.calcularPorc();
+        double descuentoUsuario = usuario.calcularPorcentajeDescuento();
+        double precio = base - descuentoFuncion - descuentoUsuario;
+
+        if (precio < 0) {
+            throw new PrecioNegativoException();
+        }
+
+        return precio;
     }
-    
-    return precio;
-}
 
-
+    /**
+     * Retorna una cadena con los detalles de la boleta, incluyendo película, usuario y precio.
+     *
+     * @return Detalle en formato legible para el usuario.
+     */
     public String mostrarDetalle() {
-        return "Boleta - Película: " + pelicula.getNombre() + ", Usuario: " + usuario.getNombre() + ", Precio Final: $" + precioFinal;
+        return "Boleta - Película: " + pelicula.getNombre()
+             + ", Usuario: " + usuario.getNombre()
+             + ", Precio Final: $" + precioFinal;
     }
 
+    /**
+     * Obtiene el precio final calculado para esta boleta.
+     *
+     * @return Precio final de la boleta.
+     */
     public double getPrecioFinal() {
         return precioFinal;
     }
 }
+
