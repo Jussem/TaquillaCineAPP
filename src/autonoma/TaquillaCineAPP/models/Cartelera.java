d699ad320c7f8c748580d4069f165fc816956b11
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-05 23:13:03 -0500

    Se comentó el codigo de la clase 'Cartelera' y 'Cine'

--- src/autonoma/TaquillaCineAPP/models/Cartelera.java
+++ src/autonoma/TaquillaCineAPP/models/Cartelera.java
@@ -7,24 +7,49 @@ package autonoma.TaquillaCineAPP.models;
 import java.util.ArrayList;
 
 /**
- *
+ * Representa la cartelera de películas disponibles en el cine.
+ * Permite agregar, eliminar y listar películas que se encuentran en exhibición.
+ * 
  * @author marti
+ * @version 20250405
+ * @since 1.0
+ * @see Pelicula
  */
 class Cartelera {
+
+    /** Lista de películas disponibles en la cartelera */
     private ArrayList<Pelicula> peliculas;
 
+    /**
+     * Crea una nueva instancia de la cartelera con una lista vacía de películas.
+     */
     public Cartelera() {
         peliculas = new ArrayList<Pelicula>();
     }
 
+    /**
+     * Agrega una película a la cartelera.
+     *
+     * @param pelicula Película que se desea agregar.
+     */
     public void agregarPelicula(Pelicula pelicula) {
         peliculas.add(pelicula);
     }
 
+    /**
+     * Elimina una película de la cartelera si se encuentra en la lista.
+     *
+     * @param pelicula Película que se desea eliminar.
+     */
     public void eliminarPelicula(Pelicula pelicula) {
         peliculas.remove(pelicula);
     }
 
+    /**
+     * Retorna la lista actual de películas disponibles en la cartelera.
+     *
+     * @return Lista de películas en exhibición.
+     */
     public ArrayList<Pelicula> listarPeliculas() {
         return peliculas;
     }
--- src/autonoma/TaquillaCineAPP/models/Cine.java
+++ src/autonoma/TaquillaCineAPP/models/Cine.java
@@ -6,45 +6,96 @@ package autonoma.TaquillaCineAPP.models;
 import java.util.ArrayList;
 import java.util.List;
 /**
- *
+ * Representa un cine que gestiona usuarios, cartelera de películas y la venta de boletas.
+ * Proporciona funcionalidades para registrar usuarios, administrar la cartelera
+ * y generar facturas a partir de ventas realizadas.
+ * 
  * @author juanb
+ * @version 20250405
+ * @since 1.0
+ * @see Usuario
+ * @see Cartelera
+ * @see Venta
+ * @see Factura
  */
 class Cine {
+
+    /** Lista de usuarios registrados en el cine */
     private ArrayList<Usuario> usuarios;
+
+    /** Cartelera de películas disponibles en el cine */
     private Cartelera cartelera;
 
+    /**
+     * Crea una nueva instancia del cine con una cartelera vacía y sin usuarios registrados.
+     */
     public Cine() {
         usuarios = new ArrayList<Usuario>();
         cartelera = new Cartelera();
     }
 
+    /**
+     * Procesa una venta de boletas y genera una factura con el total correspondiente.
+     *
+     * @param venta Venta realizada con una o más boletas.
+     * @return Factura generada a partir del total de la venta.
+     */
     public Factura venderBoletas(Venta venta) {
         double total = venta.calcularTotal();
         return new Factura(total);
     }
 
+    /**
+     * Registra un nuevo usuario en el sistema del cine.
+     *
+     * @param usuario Usuario que se desea agregar.
+     */
     public void agregarUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
 
+    /**
+     * Elimina un usuario del sistema del cine.
+     *
+     * @param usuario Usuario que se desea eliminar.
+     */
     public void eliminarUsuario(Usuario usuario) {
         usuarios.remove(usuario);
     }
 
+    /**
+     * Retorna la lista completa de usuarios registrados en el cine.
+     *
+     * @return Lista de usuarios.
+     */
     public ArrayList<Usuario> listarUsuarios() {
         return usuarios;
     }
 
+    /**
+     * Agrega una película a la cartelera del cine.
+     *
+     * @param pelicula Película que se desea agregar a la cartelera.
+     */
     public void agregarPelicula(Pelicula pelicula) {
         cartelera.agregarPelicula(pelicula);
     }
 
+    /**
+     * Elimina una película de la cartelera del cine.
+     *
+     * @param pelicula Película que se desea eliminar de la cartelera.
+     */
     public void eliminarPelicula(Pelicula pelicula) {
         cartelera.eliminarPelicula(pelicula);
     }
 
+    /**
+     * Devuelve la lista de películas actualmente en cartelera.
+     *
+     * @return Lista de películas disponibles.
+     */
     public ArrayList<Pelicula> listarPeliculas() {
         return cartelera.listarPeliculas();
     }
 }
-
