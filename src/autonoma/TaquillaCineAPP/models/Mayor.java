3bec572dc0877e05105dc7ff562c6710b2eb07bc
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-07 16:32:17 -0500

    Se comentó la clase 'Niño','Mayor'

--- src/autonoma/TaquillaCineAPP/models/Mayor.java
+++ src/autonoma/TaquillaCineAPP/models/Mayor.java
@@ -5,15 +5,36 @@
 package autonoma.TaquillaCineAPP.models;
 
 /**
- *
+ * Representa a un usuario de tipo adulto mayor en el sistema del cine.
+ * Los adultos mayores tienen derecho a un descuento especial aplicado
+ * al precio de sus boletas.
+ * 
+ * Esta clase extiende de {@link Usuario} e implementa el método para calcular
+ * el porcentaje de descuento aplicable a este tipo de usuario.
+ * 
  * @author marti
+ * @version 20250405
+ * @since 1.0
+ * @see Usuario
  */
 public class Mayor extends Usuario {
+
+    /**
+     * Crea un nuevo usuario de tipo adulto mayor con el nombre especificado.
+     *
+     * @param nombre Nombre del usuario adulto mayor.
+     */
     public Mayor(String nombre) {
         super(nombre);
     }
 
+    /**
+     * Calcula el porcentaje de descuento aplicable a un adulto mayor.
+     *
+     * @return Descuento aplicado como valor decimal (1000.0 en este caso).
+     */
     public double calcularPorcentajeDescuento() {
         return 1000.0;
     }
 }
+
